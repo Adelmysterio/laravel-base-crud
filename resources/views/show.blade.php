26f7bebf1d6a211c4277a198bcd0ec52277e32e7
AdelMysterio <storliniadelmo@gmail.com>
2024-07-22 14:13:03 +0200

    update grafico ed implementato easter egg

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,8 +4,12 @@
     <main>
         <article class="text-center">
             <h2>Pokemon #{{ $pokemon->id }} {{ $pokemon->name }}</h2>
+            @if ($pokemon->id == 6)
+                <h3>Best Pokemon Ever</h3>
+            @endif
             <img src="{{ $pokemon->img_url }}" alt="{{ $pokemon->name }}">
-            <p>{{ $pokemon->type }} {{$pokemon->ability}}</p>
+            <p>Type: {{ $pokemon->type }}</p>
+            <p>Ability: {{ $pokemon->ability }}</p>
         </article>
     </main>
 @endsection
