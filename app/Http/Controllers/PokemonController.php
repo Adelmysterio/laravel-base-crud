50ec72b932dcfa965cdc80f45624708a78bc9358
AdelMysterio <storliniadelmo@gmail.com>
2024-07-22 13:18:44 +0200

    creato migration e seedati dati all'interno

---
+++ app/Http/Controllers/PokemonController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\pokemon;
+use Illuminate\Http\Request;
+
+class PokemonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(pokemon $pokemon)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(pokemon $pokemon)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, pokemon $pokemon)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(pokemon $pokemon)
+    {
+        //
+    }
+}
